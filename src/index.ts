d08f50023b121f0e225bca54f2347102af10e499
ledsonvanini <ledsonvanini@gmail.com>
2024-07-16 19:44:08 -0300

    feat: added code style config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/server";
-const PORT = 3000 || process.env.PORT;
+const PORT = 3333;
 
 server.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`);
